d21c1e9838b70c7a2e4b24bb57621f6cc1e96151
jtlap <jtlapreste@gmail.com>
2020-11-16 11:23:31 +0100

    expx2 and expmx2

--- test/doc/core/expmx2.cpp
+++ test/doc/core/expmx2.cpp
@@ -1,4 +1,4 @@
-#include <eve/function/expx2.hpp>
+#include <eve/function/expmx2.hpp>
 #include <eve/wide.hpp>
 #include <eve/constant/inf.hpp>
 #include <eve/constant/minf.hpp>
@@ -15,13 +15,13 @@ wide_ft pf = {0.0f, -0.0f, -1.0f, 1.0f, 2.0f,
                eve::inf(eve::as<float>()), eve::minf(eve::as<float>()), eve::nan(eve::as<float>())};
 
   std::cout << "---- simd" << '\n'
-            << "<- pf        = " << pf << '\n'
-            << "-> expx2(pf) = " << eve::expx2(pf) << '\n';
+            << "<- pf         = " << pf << '\n'
+            << "-> expmx2(pf) = " << eve::expmx2(pf) << '\n';
 
   float xf = 3.0f;
 
   std::cout << "---- scalar" << '\n'
-            << "<- xf        = " << xf << '\n'
-            << "-> expx2(xf) = " << eve::expx2(xf) << '\n';
+            << "<- xf         = " << xf << '\n'
+            << "-> expmx2(xf) = " << eve::expmx2(xf) << '\n';
   return 0;
 }
