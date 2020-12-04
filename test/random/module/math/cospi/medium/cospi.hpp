51a206809b8778af3f0303e2531eb2ecd4413899
jtlap <jtlapreste@gmail.com>
2020-12-04 21:14:58 +0100

    corrective to cospi integral test

--- test/random/module/math/cospi/medium/cospi.hpp
+++ test/random/module/math/cospi/medium/cospi.hpp
@@ -10,14 +10,24 @@
 //==================================================================================================
 #include <eve/detail/function/tmp/boost_math_cospi.hpp>
 #include <eve/function/cospi.hpp>
+#include <eve/constant/valmin.hpp>
+#include <eve/constant/valmax.hpp>
 #include "producers.hpp"
 #include <cmath>
 #include <type_traits>
+#include <eve/concept/value.hpp>
 
 TTS_CASE_TPL("wide random check on cospi", EVE_TYPE)
 {
   auto my_stdcospi = [](auto x) -> EVE_VALUE {return boost::math::cos_pi(x); };
-
-  eve::uniform_prng<EVE_VALUE> p(EVE_VALUE(-1000.0), EVE_VALUE(1000.0));
-  TTS_RANGE_CHECK(p, my_stdcospi, eve::medium(eve::cospi));
+  if constexpr(eve::integral_value<EVE_VALUE>)
+  {
+    eve::uniform_prng<EVE_VALUE> p(eve::valmin(eve::as<EVE_VALUE>()), eve::valmax(eve::as<EVE_VALUE>()));
+    TTS_RANGE_CHECK(p, my_stdcospi, eve::medium(eve::cospi));
+  }
+  else
+  {
+    eve::uniform_prng<EVE_VALUE> p(EVE_VALUE(-100000.0), EVE_VALUE(100000.0));
+    TTS_RANGE_CHECK(p, my_stdcospi, eve::medium(eve::cospi));
+  }
 }
